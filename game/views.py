d95fbd125df839712ca976758ddc62d44535a813
Vlasov Vitaly <vnigtha@gmail.com>
2017-11-25 18:17:05 +0300

    Index view with empty template.

--- game/views.py
+++ game/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def start_page(request):
+    context = {}
+
+    return render(request, 'game/start_page.html')
