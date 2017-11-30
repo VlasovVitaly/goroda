3f54b93bbc1c39fc77de7922ab0ce0dc79937017
Vlasov Vitaly <vnigtha@gmail.com>
2017-11-30 12:47:59 +0300

    Start new match view working.

--- game/views.py
+++ game/views.py
@@ -13,4 +13,13 @@ def start_page(request):
 
 @login_required
 def start_new_match(request):
-    return render(request, 'game/start_new_match.html')
+    context = {}
+    context['form'] = start_form = StartNewMatchForm(request.POST or None)
+
+    if start_form.is_valid():
+        match = start_form.save(commit=False)
+        match.judge = request.user
+        match.save()
+        # TODO redirect to match details
+
+    return render(request, 'game/start_new_match.html', context=context)
