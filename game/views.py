07026e4aa1002f038c193fe752201d0f3eaf49f8
Vlasov Vitaly <vnigtha@gmail.com>
2017-11-30 12:19:17 +0300

    Start new match form with empty view.

--- game/views.py
+++ game/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .forms import StartNewMatchForm
 
 @login_required
 def start_page(request):
     context = {}
 
     return render(request, 'game/start_page.html')
+
+
+@login_required
+def start_new_match(request):
+    pass
