52ccde9f0ae33563ade6a0362346a154b966f7fc
Vitaly Vlasov <vnigtha@gmail.com>
2017-11-30 13:53:25 +0300

    Added Turn and Match models in admin site.

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import City
+from .models import City, Match, Turn
 
 
 admin.site.register(City)
+admin.site.register(Match)
+admin.site.register(Turn)
