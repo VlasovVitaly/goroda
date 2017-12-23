86af8af16668c0e32baadeb95634883380476423
Vitaly Vlasov <vnigtha@gmail.com>
2017-12-23 15:28:45 +0300

    Initial gettext translations.

--- goroda/settings.py
+++ goroda/settings.py
@@ -82,6 +82,8 @@ LANGUAGE_CODE = 'ru-ru'
 USE_I18N = True
 USE_L10N = True
 
+LOCALE_PATHS = [os.path.join(BASE_DIR, 'locales')]
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
