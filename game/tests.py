331411a23be924763b4ac3235cddb536c7f615fc
Vitaly Vlasov <vnigtha@gmail.com>
2018-01-25 18:23:25 +0300

    Initial City model test.
      Added 1 test for unique values

--- game/tests.py
+++ game/tests.py
@@ -1,3 +1,16 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import City
+
+
+class CityModelTest(TestCase):
+    def testUnique(self):
+        """All city names must be unique."""
+        first = City(name='TEST_NAME')
+        first.save()
+
+        second = City(name='TEST_NAME')
+        self.assertRaises(ValidationError, second.full_clean)
+
+        first.delete()
