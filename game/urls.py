8dbbab14ccc31ca063e97d19b44bb9c36056eb32
Vitaly Vlasov <vnigtha@gmail.com>
2018-03-28 15:19:56 +0300

    Removed word from url route

--- game/urls.py
+++ game/urls.py
@@ -6,6 +6,6 @@ app_name = 'game'
 
 urlpatterns = [
     path('start/', views.start_new_match, name='start'),
-    path('<int:match_id>/detail', views.match_detail, name='detail'),
+    path('<int:match_id>/', views.match_detail, name='detail'),
     path('<int:match_id>/end', views.end_match, name='end'),
 ]
