66bf2a18d5258b53324d526aa3fa58939f7efc93
Vitaly Vlasov <vnigtha@gmail.com>
2018-03-30 17:47:11 +0300

    Letters statistic management command

---
+++ game/management/commands/letters_stats.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+
+from django.core.management.base import BaseCommand
+
+from game.models import City
+
+
+class Command(BaseCommand):
+    help = 'Display all lettest statistics'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        cities_qs = City.objects.filter(geotype=City.GEOTYPE_CITY).only('name')
+        city_names = {cty.name for cty in cities_qs}
+
+        letter_counters = defaultdict(lambda: 0)
+
+        for city in city_names:
+            letter_counters[city[0].upper()] += 1
+
+        for letter in sorted(letter_counters.keys()):
+            print(letter, letter_counters[letter])
