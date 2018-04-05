aefde16d1ef77d557f92eb52c73d29f1acc9a075
Vitaly Vlasov <vnigtha@gmail.com>
2018-04-05 18:04:06 +0300

    Added test to Match model

--- game/tests.py
+++ game/tests.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-from .models import City
+from .models import City, Match
 
 
 class CityModelTest(TestCase):
@@ -23,3 +24,27 @@ class CityModelTest(TestCase):
 
         self.assertRaises(ValidationError, test_city.full_clean)
         self.assertRaises(ValidationError, test_other.full_clean)
+
+
+class MatchModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.test_username = 'judge'
+        cls.test_user = User.objects.create_user(cls.test_username)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+
+        cls.test_user.delete()
+
+    def test_next_team_prop(self):
+        match = Match(judge=self.test_user)
+
+        match.current_team = 1
+        self.assertEqual(match.next_team, 2)
+
+        match.current_team = 2
+        self.assertEqual(match.next_team, 1)
