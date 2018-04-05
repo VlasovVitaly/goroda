febeb8606fdd184d1bdacc102e4a3f4a650e8a2e
Vitaly Vlasov <vnigtha@gmail.com>
2018-04-05 18:07:01 +0300

    Removed unused import

--- game/views.py
+++ game/views.py
@@ -1,7 +1,6 @@
 from django.db import transaction
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
 
 from .decorators import match_judge_required
 from .forms import StartNewMatchForm, TurnForm
